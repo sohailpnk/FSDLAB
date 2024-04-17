87dd9e4a246834d21f92497f88973f2ec2a5efbc
sohailpnk <162548214+sohailpnk@users.noreply.github.com>
2024-04-17 22:42:45 +0530

    Update App.js

--- dbschema/src/App.js
+++ dbschema/src/App.js
@@ -23,3 +23,10 @@ function App() {
 }
 
 export default App;
+/* node server1.js
+Invoke-RestMethod -Method Post -Uri "http://localhost:5000/api/users" -ContentType "application/json" -Body '{"name": "John Doe", "email": "john@example.com", "age": 30}'
+Invoke-RestMethod -Method Post -Uri "http://localhost:5000/api/users" -ContentType "application/json" -Body '{"name": "Alex", "email": "john@yahoo.com", "age": 35}'
+Invoke-RestMethod -Method Get -Uri "http://localhost:5000/api/users"
+Invoke-RestMethod -Method Put -Uri "http://localhost:5000/api/users/65cb829decab01d45546620b" -ContentType "application/json" -Body '{"name": "Siri", "email": "siri@in.com", "age": 25}'
+Invoke-RestMethod -Method Delete -Uri "http://localhost:5000/api/users/65cb821aecab01d455466209"
+*/
